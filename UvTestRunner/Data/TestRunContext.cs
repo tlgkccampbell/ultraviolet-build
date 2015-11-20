b40a726edf7507684f8465be23fd3f7ca6f42232
Cole Campbell <cole.campbell@twistedlogik.net>
2015-11-20 16:21:02 -0500

    Moved build toolchain projects into their own directory.

---
+++ UvTestRunner/Data/TestRunContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using UvTestRunner.Models;
+
+namespace UvTestRunner.Data
+{
+    public class TestRunContext : DbContext
+    {
+        static TestRunContext()
+        {
+            Database.SetInitializer<TestRunContext>(null);
+        }
+
+        public TestRunContext()
+            : base("TestRunContext")
+        {
+
+        }
+
+        public DbSet<TestRun> TestRuns { get; set; }
+    }
+}
